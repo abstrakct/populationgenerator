429ecee090a11ca9c5a37d99958f33e77711d62b
Rolf K <abstractstar@gmail.com>
2017-10-07 01:59:09 +0200

    Trying to remove build files..

--- src/version.h
+++ src/version.h
@@ -1,8 +1,8 @@
 #ifndef VERSION_H
 #define VERSION_H
-#define VERSION_MAJOR a43b46f-dirty
+#define VERSION_MAJOR 585c3df-dirty
 #define VERSION_MINOR 
 #define VERSION_REVISION 
 #define VERSION_HASH ""
-#define VERSION_STRING "a43b46f-dirty..."
+#define VERSION_STRING "585c3df-dirty..."
 #endif
