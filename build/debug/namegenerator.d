1e64eaf85264a58866e4321bbe69101f68b87f80
Rolf K <abstractstar@gmail.com>
2017-10-11 20:01:14 +0200

    Removed some build files.

--- build/debug/namegenerator.d
+++
@@ -1,8 +0,0 @@
-build/debug/namegenerator.o: src/namegenerator.cpp src/debug.h \
- src/utils.h src/namegenerator.h
-
-src/debug.h:
-
-src/utils.h:
-
-src/namegenerator.h:
--- src/version.h
+++ src/version.h
@@ -1,8 +1,8 @@
 #ifndef VERSION_H
 #define VERSION_H
-#define VERSION_MAJOR 8f311ab-dirty
+#define VERSION_MAJOR 89cb985-dirty
 #define VERSION_MINOR 
 #define VERSION_REVISION 
 #define VERSION_HASH ""
-#define VERSION_STRING "8f311ab-dirty..."
+#define VERSION_STRING "89cb985-dirty..."
 #endif
