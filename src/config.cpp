76912700e5535e204cf32ece28299634ee7465a6
Rolf K <abstractstar@gmail.com>
2020-08-27 21:27:33 +0200

    Fix config parser

--- src/config.cpp
+++ src/config.cpp
@@ -11,26 +11,36 @@ void readConfigFiles()
 {
     std::ifstream ifs(CONFIG_FILE);
     Json::CharReaderBuilder builder;
-    Json::CharReader *reader = builder.newCharReader();
+    // Json::CharReader *reader = builder.newCharReader();
+    std::unique_ptr<Json::CharReader> const reader(builder.newCharReader());
     Json::Value root;
     std::string errors;
 
+    std::cout << "Reading config file: " << CONFIG_FILE << std::endl;
+
     ifs >> root;
-    reader->parse(root.asString().c_str(), root.asString().c_str() + root.asString().size(), &root, &errors);
-    Json::Value& simulationoptions = root["simulationoptions"];
 
-    // TODO: add error checking / try/catch exceptions 
+    // std::cout << root << std::endl;
 
-    c.yearZero = simulationoptions[0]["yearZero"].asInt();
-    c.simulationYears = simulationoptions[0]["simulationYears"].asInt();
-    c.initialPopulationMin = simulationoptions[0]["initialPopulationMin"].asInt();
-    c.initialPopulationMax = simulationoptions[0]["initialPopulationMax"].asInt();
-    c.ageAdult = simulationoptions[0]["ageAdult"].asInt();
-    c.marriageFrequency = simulationoptions[0]["marriageFrequency"].asInt();
-    c.sexyTimeFrequency = simulationoptions[0]["sexyTimeFrequency"].asInt();
-    c.unexpectedDeathFrequency = simulationoptions[0]["unexpectedDeathFrequency"].asInt();
-    c.oldAgeDeathFrequency = simulationoptions[0]["oldAgeDeathFrequency"].asInt();
-    c.childBirthDeathFrequency = simulationoptions[0]["childBirthDeathFrequency"].asInt();
-    c.immigrationFrequency = simulationoptions[0]["immigrationFrequency"].asInt();
+    char const *begin, *end;
+    root.getString(&begin, &end);
+    reader->parse(begin, end, &root, &errors);
+
+    Json::Value &options = root["options"];
+
+    // std::cout << options["simulation"][0]["yearZero"] << std::endl;
+
+    // TODO: add error checking / try/catch exceptions
+
+    c.yearZero = options["simulation"][0]["yearZero"].asInt();
+    c.simulationYears = options["simulation"][0]["simulationYears"].asInt();
+    c.initialPopulationMin = options["simulation"][0]["initialPopulationMin"].asInt();
+    c.initialPopulationMax = options["simulation"][0]["initialPopulationMax"].asInt();
+    c.ageAdult = options["simulation"][0]["ageAdult"].asInt();
+    c.marriageFrequency = options["simulation"][0]["marriageFrequency"].asInt();
+    c.sexyTimeFrequency = options["simulation"][0]["sexyTimeFrequency"].asInt();
+    c.unexpectedDeathFrequency = options["simulation"][0]["unexpectedDeathFrequency"].asInt();
+    c.oldAgeDeathFrequency = options["simulation"][0]["oldAgeDeathFrequency"].asInt();
+    c.childBirthDeathFrequency = options["simulation"][0]["childBirthDeathFrequency"].asInt();
+    c.immigrationFrequency = options["simulation"][0]["immigrationFrequency"].asInt();
 }
-
