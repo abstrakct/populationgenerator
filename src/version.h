f5536781e810bd58d9b1dfed92f558447f5b552a
Rolf K <abstractstar@gmail.com>
2020-10-18 11:21:27 +0200

    remove --dirty flag from git describe

--- src/version.h
+++ src/version.h
@@ -1,8 +1,8 @@
 #ifndef VERSION_H
 #define VERSION_H
-#define VERSION_MAJOR 0
-#define VERSION_MINOR 1
-#define VERSION_REVISION 1
-#define VERSION_HASH "gfe29779-dirty"
-#define VERSION_STRING "0.1.1.gfe29779-dirty"
+#define VERSION_MAJOR 
+#define VERSION_MINOR 
+#define VERSION_REVISION 
+#define VERSION_HASH ""
+#define VERSION_STRING "..."
 #endif
