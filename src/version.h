603e6b40064b20681316f30fb5f10e20fd85d358
Rolf K <abstractstar@gmail.com>
2020-10-18 11:19:20 +0200

    version

--- src/version.h
+++ src/version.h
@@ -2,7 +2,7 @@
 #define VERSION_H
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 1
-#define VERSION_REVISION 0
-#define VERSION_HASH "g4afd76b-dirty"
-#define VERSION_STRING "0.1.0.g4afd76b-dirty"
+#define VERSION_REVISION 1
+#define VERSION_HASH "gfe29779-dirty"
+#define VERSION_STRING "0.1.1.gfe29779-dirty"
 #endif
